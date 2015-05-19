eda98270e34a2f8e8ff0d27e3d60344a1caec28e
Filip Fafara <ffafara@thoughtworks.com>
2015-05-19 14:49:59 -0400

    added glance view

--- pushtest WatchKit Extension/GlanceController.swift
+++ pushtest WatchKit Extension/GlanceController.swift
@@ -11,11 +11,18 @@ import Foundation
 
 
 class GlanceController: WKInterfaceController {
+    
+    @IBOutlet var estLabel: WKInterfaceLabel? = WKInterfaceLabel()
+    @IBOutlet var cstLabel: WKInterfaceLabel? = WKInterfaceLabel()
+    @IBOutlet var mstLabel: WKInterfaceLabel? = WKInterfaceLabel()
+    @IBOutlet var gmtLabel: WKInterfaceLabel? = WKInterfaceLabel()
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
         // Configure interface objects here.
+        var timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("updateClocks"), userInfo: nil, repeats: true)
+        timer.fire()
     }
 
     override func willActivate() {
@@ -26,6 +33,50 @@ class GlanceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    func updateClocks() {
+        var time: NSDate = NSDate()
+        
+        let formatter:NSDateFormatter = NSDateFormatter();
+        var timeZone = NSTimeZone(name: "UTC")
+        formatter.timeZone = timeZone
+        formatter.dateFormat = "h:mm a"
+        var formattedString = formatter.stringFromDate(time)
+        var formatDateString = formattedString.stringByReplacingOccurrencesOfString(" p", withString: "PM", options: nil, range: nil)
+        formattedString = formattedString.stringByReplacingOccurrencesOfString(" a", withString: "AM", options: nil, range: nil)
+        
+        timeZone = NSTimeZone(name: "US/Eastern")
+        formatter.timeZone = timeZone
+        formatter.dateFormat = "h:mm a"
+        formattedString = formatter.stringFromDate(time)
+        formatDateString = formattedString.stringByReplacingOccurrencesOfString(" p", withString: "PM", options: nil, range: nil)
+        formattedString = formattedString.stringByReplacingOccurrencesOfString(" a", withString: "AM", options: nil, range: nil)
+        self.estLabel?.setText(formatDateString)
+        
+        timeZone = NSTimeZone(name: "UTC")
+        formatter.timeZone = timeZone
+        formatter.dateFormat = "h:mm a"
+        formattedString = formatter.stringFromDate(time)
+        formatDateString = formattedString.stringByReplacingOccurrencesOfString(" p", withString: "PM", options: nil, range: nil)
+        formattedString = formattedString.stringByReplacingOccurrencesOfString(" a", withString: "AM", options: nil, range: nil)
+        self.gmtLabel?.setText(formatDateString)
+        
+        timeZone = NSTimeZone(name: "US/Mountain")
+        formatter.timeZone = timeZone
+        formatter.dateFormat = "h:mm a"
+        formattedString = formatter.stringFromDate(time)
+        formatDateString = formattedString.stringByReplacingOccurrencesOfString(" p", withString: "PM", options: nil, range: nil)
+        formattedString = formattedString.stringByReplacingOccurrencesOfString(" a", withString: "AM", options: nil, range: nil)
+        self.mstLabel?.setText(formatDateString)
+        
+        timeZone = NSTimeZone(name: "US/Central")
+        formatter.timeZone = timeZone
+        formatter.dateFormat = "h:mm a"
+        formattedString = formatter.stringFromDate(time)
+        formatDateString = formattedString.stringByReplacingOccurrencesOfString(" p", withString: "PM", options: nil, range: nil)
+        formattedString = formattedString.stringByReplacingOccurrencesOfString(" a", withString: "AM", options: nil, range: nil)
+        self.cstLabel?.setText(formatDateString)
     }
 
 }
